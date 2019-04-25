4cd01ac0041ea4f4c596be7ed5221bad8bd6b562
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-04-24 19:36:54 -0700

    Update and delete code for proxy deployment testing

--- server/index.js
+++ server/index.js
@@ -11,20 +11,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.all('/house_images', (req, res) => {
-  proxy.web(req, res, { target: 'http://localhost:3003' });
-});
+// app.all('/house_images', (req, res) => {
+//   proxy.web(req, res, { target: 'http://localhost:3003' });
+// });
 
-app.all('/description', (req, res) => {
-  proxy.web(req, res, { target: 'http://localhost:3210' });
-});
+// app.all('/description', (req, res) => {
+//   proxy.web(req, res, { target: 'http://localhost:3210' });
+// });
 
-app.all('/totalReviews', (req, res) => {
-  proxy.web(req, res, { target: 'http://localhost:3004' });
-});
+// app.all('/totalReviews', (req, res) => {
+//   proxy.web(req, res, { target: 'http://localhost:3004' });
+// });
+
+// app.all('/morehomes', (req, res) => {
+//   proxy.web(req, res, { target: 'http://localhost:3000' });
+// });
 
 app.all('/morehomes', (req, res) => {
-  proxy.web(req, res, { target: 'http://localhost:3000' });
+  proxy.web(req, res, { target: 'http://52.88.112.117:3000/' });
 });
 
 const server = app.listen(port, console.log(`Listening on port ${port}`));
