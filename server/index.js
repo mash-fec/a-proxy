a7f3512ea5d985e8c8d7d899ffdeac8f3ffab1df
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-04-27 13:12:23 -0700

    Remove unused code

--- server/index.js
+++ server/index.js
@@ -11,21 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// app.all('/house_images', (req, res) => {
-//   proxy.web(req, res, { target: 'http://localhost:3003' });
-// });
-
-// app.all('/description', (req, res) => {
-//   proxy.web(req, res, { target: 'http://localhost:3210' });
-// });
-
-// app.all('/totalReviews', (req, res) => {
-//   proxy.web(req, res, { target: 'http://localhost:3004' });
-// });
-
-// app.all('/morehomes', (req, res) => {
-//   proxy.web(req, res, { target: 'http://localhost:3000' });
-// });
 app.all('/house_images', (req, res) => {
   proxy.web(req, res, { target: 'http://ec2-54-175-40-167.compute-1.amazonaws.com:3003/' });
 });
@@ -34,9 +19,9 @@ app.all('/description', (req, res) => {
   proxy.web(req, res, { target: 'http://18.220.111.23:3210/' });
 });
 
-// app.all('/totalReviews', (req, res) => {
-//   proxy.web(req, res, { target: 'http://reviews.pfuzgfpajh.us-west-2.elasticbeanstalk.com/' });
-// });
+app.all('/totalReviews', (req, res) => {
+  proxy.web(req, res, { target: 'http://reviews.pfuzgfpajh.us-west-2.elasticbeanstalk.com/' });
+});
 
 app.all('/morehomes', (req, res) => {
   proxy.web(req, res, { target: 'http://52.88.112.117:3000/' });
